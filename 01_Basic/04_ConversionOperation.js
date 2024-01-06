a87b0b18beb98c049e4a2d47133e371057dba6bb
Abhay Singh Kachwaha <93095229+Abhayyyyyyy@users.noreply.github.com>
2024-01-06 06:04:07 +0000

    Conversion Operation Part 2

---
+++ 01_Basic/04_ConversionOperation.js
@@ -0,0 +1,28 @@
+let value = 3
+let negVal = -value
+
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Virus"
+let str3 = str1 + str2
+//console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2");
+console.log(1 + "2" + 2);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+//link to Study
+// https://tc39.es/ecma262/multipage/abstract-operation.html#sec-type-conversion
