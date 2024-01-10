71fa4e41644c8396f8704aa2f68ee7339d534d15
Abhay Singh Kachwaha <93095229+Abhayyyyyyy@users.noreply.github.com>
2024-01-10 06:21:34 +0000

    DataType Summary and Memory

--- 01_Basic/06_DataType.js
+++ 01_Basic/06_DataType.js
@@ -28,6 +28,33 @@ let myObj = {                                //Objects
 const myFun = function(){
     console.log("Hello World");
 }
- console.log(typeof myFun );
+// console.log(typeof myFun );
 
  // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+ // ################################################################################################
+
+                                //******Memory******
+
+// Stack (Primitive DataTypes) copy data, Heap (Non-Primitive DataType) reference data
+
+let myYoutubeName = "Abhayy"
+
+let anotherName = myYoutubeName
+anotherName = "Chai&Code"
+
+//console.log(myYoutubeName);
+//console.log(anotherName);
+
+let userOne = {
+    email: "abc@gmail.com",
+    upi: "user@ybl"
+}
+console.log(userOne.email);
+let userTwo = userOne
+userTwo.email = "xyz@rediff.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
